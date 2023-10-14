97efa7029c1f6a8cedb22bc27956b1e6c91dbf00
Srigovind Nayak <sgovind.dev@outlook.com>
2023-10-14 23:17:34 +0530

    perl numbers

---
+++ 003-data-types/numbers.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+my $x = 20;
+my $y = 30;
+my $z = -30;
+
+# to make numbers more readable, you can use _ instead of ,
+# , is used in lists
+
+my $l = 123_456_789;
+print($l, "\n");
+
+# floating point numbers
+
+# The floating-point number holds 8 bytes, with 11 bits reserved for the exponent and 53 bits for significand.
+
+# The range of floating-point numbers is essentially determined by the standard C library of the underlying platform where Perl is running.
